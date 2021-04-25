6bbc11bb2fa386cb51729a504607543cb3983098
Ignacio Barletta Trípodi <IgnacioBarletta@users.noreply.github.com>
2021-04-24 23:15:54 -0300

    Subo archivos

--- app.js
+++ app.js
@@ -10,38 +10,37 @@ app.listen(3030, () => {
 // Defino carpeta que devuelve archivos estáticos
 app.use(express.static('public'))
 
-
 // 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/babbage.html'));
+app.get('/babbage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/babbage.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/berners-lee.html'));
+app.get('/berners-lee', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/berners-lee.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/clarke.html'));
+app.get('/clarke', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/clarke.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hamilton.html'));
+app.get('/hamilton', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/hamilton.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/hopper.html'));
+app.get('/hopper', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/hopper.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/lovelace.html'));
+app.get('/lovelace', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/lovelace.html'));
 }); 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/turing.html'));
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/turing.html'));
 });
 
 // Defino status y mensaje para una página no encontrada
