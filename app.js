1cd7752247abc626bfa30b65fe276a2df0bd57c9
Ignacio Barletta Trípodi <IgnacioBarletta@users.noreply.github.com>
2021-04-24 22:54:50 -0300

    Subo los archivos

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express ();
 
+// Inicio servidor
 app.listen(3030, () => {
     console.log("El servidor está corriendo");
 });
 
+// Defino carpeta que devuelve archivos estáticos
 app.use(express.static('public'))
 
+
+// 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/babagge.html'));
+    res.sendFile(path.join(__dirname, '/views/babbage.html'));
 });
 
 app.get('/', (req, res) => {
@@ -39,3 +43,8 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/turing.html'));
 });
+
+// Defino status y mensaje para una página no encontrada
+app.get('*', (req, res) => {
+    res.status(404  ).send('Página no encontrada')
+})
